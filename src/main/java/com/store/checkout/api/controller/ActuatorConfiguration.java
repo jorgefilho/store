077c9e025c50a10c37a1a64dab3a25e433fcd76f
jorgefilho <papachristo@gmail.com>
2016-06-18 12:09:16 -0300

    Improvements.

---
+++ src/main/java/com/store/checkout/api/controller/ActuatorConfiguration.java
@@ -0,0 +1,10 @@
+package com.store.checkout.api.controller;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+public class ActuatorConfiguration {
+
+}
